b558e2fca35dfdce235faa16a4630fb66c8efd96
C:/Program Files/Git/CloudNine <kurtrussellquiogue25@gmail.com>
2022-11-21 09:55:16 +0800

    UPDATED

--- resources/views/users/signin.blade.php
+++ resources/views/users/signin.blade.php
@@ -52,22 +52,22 @@
                                     <label class="form-label" for="email">Email</label>
                                     <input type="email" class="form-control form-control-lg" name="email"
                                         value="{{ old('email') }}" />
-                                    <span class="text-danger">
+                                    {{-- <span class="text-danger">
                                         @error('email')
                                             {{ $message }}
                                         @enderror
-                                    </span>
+                                    </span> --}}
                                 </div>
 
                                 <div class="form-outline form-white mb-4">
                                     <label class="form-label" for="password">Password</label>
                                     <input type="password" class="form-control form-control-lg" name="password"
                                         value="{{ old('password') }}">
-                                    <span class="text-danger">
+                                    {{-- <span class="text-danger">
                                         @error('password')
                                             {{ $message }}
                                         @enderror
-                                    </span>
+                                    </span> --}}
                                 </div>
 
                                 <p class="small mb-5 pb-lg-2"><a class="text-dark-50" href="#!">Forgot
@@ -76,12 +76,12 @@
 
                                 <button class="btn btn-outline-dark btn-lg px-5" type="submit">Login</button>
 
-                                <div class="d-flex justify-content-center text-center mt-2 pt-1">
+                                {{-- <div class="d-flex justify-content-center text-center mt-2 pt-1">
                                     <a href="#!" class="text-dark"><i class="fab fa-facebook-f fa-lg"></i></a>
                                     <a href="#!" class="text-dark"><i
                                             class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
                                     <a href="#!" class="text-dark"><i class="fab fa-google fa-lg"></i></a>
-                                </div>
+                                </div> --}}
 
                             </div>
 
@@ -90,7 +90,7 @@
                                         class="text-dark-50 fw-bold">Sign Up</a>
                                 </p>
                             </div>
-
+                            <br>
                             <div>
                                 <a href="{{ route('dashboard') }}" class="text-dark">
                                     <i class="fa-solid fa-home icon"></i>
